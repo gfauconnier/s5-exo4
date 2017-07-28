1f56830ac764aecb97af4f57c226e906df69413d
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-28 11:17:55 +0200

    main.js : lots of tries, I have something working, not sure if it's how it's wanted

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+displayClients("");
+
+
+$("select").change(function() {
+  if ($("select option:selected").val() != "sort") {
+    displayClients($("select option:selected").val());
+  }
+});
+
+function displayClients(param) {
+  var tabContent = "";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        clientFile = JSON.parse(this.responseText);
-        for (x in clientFile.clients) {
-          $("#clienttable thead").append("<tr><td>" + clientFile.clients[x].fname + "</td><td>" +
-            clientFile.clients[x].lname + "</td><td>" + clientFile.clients[x].age + "</td><td>" +
-            clientFile.clients[x].city + "</td></tr>");
+      clientFile = JSON.parse(this.responseText);
+
+      if (param != "") {
+        clientFile.clients.sort(function(a, b) {
+          if (a[param] < b[param]) {
+            return -1;
+          } else if (a[param] > b[param]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
       }
+
+      for (x in clientFile.clients) {
+        tabContent += "<tr><td>" + clientFile.clients[x].fname + "</td><td>" + clientFile.clients[x].lname +
+          "</td><td>" + clientFile.clients[x].age + "</td><td>" + clientFile.clients[x].city + "</td></tr>"
+      }
+      $("#clienttable tbody").html(tabContent);
+      tabContent = "";
     }
-};
-xmlhttp.open("GET", "js/clients.json", true);
-xmlhttp.send();
+  };
+  xmlhttp.open("GET", "js/clients.json", true);
+  xmlhttp.send();
+}
