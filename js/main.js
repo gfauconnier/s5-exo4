ac42f8dc1d197917b3fa8cf833c57039ecd4c31e
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-08-23 09:59:31 +0200

    removed unused older code

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-/*displayClients("");
-
- $("select").change(function() {
-   if ($("select option:selected").val() != "sort") {
-     displayClients($("select option:selected").val());
-   }
- });
-
- function displayClients(param) {
-   var tabContent = "";
-   var xmlhttp = new XMLHttpRequest();
-   xmlhttp.onreadystatechange = function() {
-     if (this.readyState == 4 && this.status == 200) {
-       clientFile = JSON.parse(this.responseText);
-
-       if (param != "") {
-         clientFile.clients.sort(function(a, b) {
-           if (a[param] < b[param]) {
-             return -1;
-           } else if (a[param] > b[param]) {
-             return 1;
-           } else {
-             return 0;
-           }
-         });
-       }
-
-       for (x in clientFile.clients) {
-         tabContent += "<tr><td>" + clientFile.clients[x].fname + "</td><td>" + clientFile.clients[x].lname +
-           "</td><td>" + clientFile.clients[x].age + "</td><td>" + clientFile.clients[x].city + "</td></tr>"
-       }
-       $("#clienttable tbody").html(tabContent);
-     }
-   };
-   xmlhttp.open("GET", "js/clients.json", true);
-   xmlhttp.send();
- }
-*/
-
-// ########################################################################################
-// ########################################################################################
-// ########################################################################################
-
 function clientsManager(){
   this.displayClients = function(){
     var tabContent = "";
@@ -76,7 +33,6 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     clientsFile = JSON.parse(this.responseText);
-
     clientManager.sortClients();
   }
 };
