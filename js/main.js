7e6f09bf3e90f0c1afb2e80b4e412ae6a27d0f6c
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-08-23 13:49:39 +0200

    added form to add (temporarily) items to the list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-function clientsManager(){
-  this.displayClients = function(){
+function clientsManager() {
+  this.displayClients = function() {
     var tabContent = "";
     for (x in clientsFile.clients) {
       tabContent += "<tr><td>" + clientsFile.clients[x].fname + "</td><td>" + clientsFile.clients[x].lname +
@@ -24,10 +24,25 @@ function clientsManager(){
     }
     this.displayClients();
   }
+  this.addClient = function() {
+    // adds lines temporary to the parsed .json content
+    clientsFile.clients.push({
+      fname: document.getElementsByClassName("inputAdd")[0].value,
+      lname: document.getElementsByClassName("inputAdd")[1].value,
+      age: document.getElementsByClassName("inputAdd")[2].value,
+      city: document.getElementsByClassName("inputAdd")[3].value
+    });
+    this.displayClients();
+  }
 }
 
 var clientManager = new clientsManager();
-document.getElementById("selectVal").addEventListener("change", function(){clientManager.sortClients();});
+document.getElementById("selectVal").addEventListener("change", function() {
+  clientManager.sortClients();
+});
+document.getElementById("buttonAdd").addEventListener("click", function() {
+  clientManager.addClient();
+})
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
